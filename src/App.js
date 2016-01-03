a599021b7a5b9f6b1dc604b6907b7c3be76b7615
Duncan Finney <duncan.finney@gmail.com>
2016-01-03 05:41:46 -0500

    Initial left/right concept

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
-import React from 'react'
+import './style.css';
+import 'fixed-data-table/dist/fixed-data-table.min.css';
+
+import React from 'react';
+import SplitPane from 'react-split-pane';
+import JSONTree from 'react-json-tree';
+import GridExample from './GridExample';
+
+// Inside a React component:
+const json = {
+  array: [1, 2, 3],
+  bool: true,
+  object: {
+    foo: 'bar'
+  }
+};
 
 export default React.createClass({
+
+  getInitialState() {
+    return {
+      leftSideWidth: localStorage.getItem('splitPos'),
+      height: window.innerHeight
+    }
+  },
+
+  onChangeSplitPaneSize(size) {
+    localStorage.setItem('splitPos', size);
+    this.setState({
+      leftSideWidth: size
+    })
+  },
+
   render() {
     return (
-      <div>
-        <h2>Welcome to React12345678</h2>
-      </div>
+      <SplitPane
+        split="vertical"
+        minSize="50"
+        defaultSize={ localStorage.getItem('splitPos') }
+        onChange={ this.onChangeSplitPaneSize }
+      >
+        <GridExample
+          width={this.state.leftSideWidth}
+          height={this.state.height}
+        />
+        <JSONTree data={json}/>
+      </SplitPane>
     )
   }
 })
